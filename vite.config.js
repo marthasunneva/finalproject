a623001e21e52cdb6936d37f4e66c1e5a2325c5c
marthasunneva <eaa24mshel@students.eaaa.dk>
2025-05-26 18:36:12 +0200

    final changes to vite.config.js for paths

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  // serve & build everything with *relative* paths:
-  base: './',
+  base: mode === 'production'
+    ? '/finalproject/'  //serving from /finalproject/
+    : '/',
   build: {
-    // bump the chunk warning limit if you like
     chunkSizeWarningLimit: 1024
   }
-})
+}))
